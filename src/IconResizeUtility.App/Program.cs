c208e380c10540f3769b9b4394644b17434688da
Manuel Eugster <manuel.eugster@hispeed.ch>
2021-02-18 21:41:47 +0100

    Added missing files

---
+++ src/IconResizeUtility.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Reflection;
+
+namespace IconResizeUtility.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand();
+
+            Command versionCommand = new Command("version");
+            versionCommand.Handler = CommandHandler.Create(GetVersion);
+            rootCommand.AddCommand(versionCommand);
+            
+            // Parse the incoming args and invoke the handler
+            rootCommand.InvokeAsync(args).Wait();
+        }
+
+        private static void GetVersion()
+        {
+            string version = Assembly.GetAssembly(typeof(Program)).GetName().Version.ToString();
+            Console.WriteLine($"Version: {version}");
+        }
+    }
+}
