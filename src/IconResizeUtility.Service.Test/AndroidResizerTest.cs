7d8be1153239166d11d042ba6527d87b7b582da4
Manuel Eugster <manuel.eugster@hispeed.ch>
2021-02-19 17:59:01 +0100

    Added android Resizer

---
+++ src/IconResizeUtility.Service.Test/AndroidResizerTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IconResizeUtility.Service.Test
+{
+    public class AndroidResizerTest
+    {
+        private ImageResizer _imageResizer;
+        private RenameUtility _renameUtility;
+
+        private string SrcDataDir
+        {
+            get
+            {
+                return Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Icons");
+            }
+        }
+
+
+        private string OutDir
+        {
+            get
+            {
+                return Path.Combine(TestContext.CurrentContext.WorkDirectory, "out", "Icons");
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _imageResizer = new ImageResizer();
+            _renameUtility = new RenameUtility();
+
+            if (Directory.Exists(OutDir))
+            {
+                Directory.Delete(OutDir, true);
+            }
+        }
+
+
+        [Test]
+        public void TestResize()
+        {
+            AndroidResizeService service = new AndroidResizeService(_imageResizer, _renameUtility);
+
+            IList<int> expectedResolutions = AndroidResizeService.DefaultRequiredSizes;
+            string expectedPrefix = "ic_";
+            const bool postFixSize = true;
+
+            service.Resize(SrcDataDir, OutDir, postFixSize, expectedPrefix, expectedResolutions);
+
+            AssertIconsExistAndMatchSize(SrcDataDir, OutDir, expectedResolutions, postFixSize, expectedPrefix);
+            AssertIconCount(SrcDataDir, OutDir, expectedResolutions);
+        }
+
+        [Test]
+        public void TestResizeWithoutPrefix()
+        {
+            AndroidResizeService service = new AndroidResizeService(_imageResizer, _renameUtility);
+
+            IList<int> expectedResolutions = AndroidResizeService.DefaultRequiredSizes;
+            string expectedPrefix = "";
+            const bool postFixSize = true;
+
+            service.Resize(SrcDataDir, OutDir, postFixSize, expectedPrefix, expectedResolutions);
+
+            AssertIconsExistAndMatchSize(SrcDataDir, OutDir, expectedResolutions, postFixSize, expectedPrefix);
+            AssertIconCount(SrcDataDir, OutDir, expectedResolutions);
+        }
+
+        [Test]
+        public void TestResizeWithoutPostfix()
+        {
+            AndroidResizeService service = new AndroidResizeService(_imageResizer, _renameUtility);
+
+            IList<int> expectedResolutions = new List<int>{48};
+            string expectedPrefix = "ic_";
+            const bool postFixSize = false;
+
+            service.Resize(SrcDataDir, OutDir, postFixSize, expectedPrefix, expectedResolutions);
+
+            AssertIconsExistAndMatchSize(SrcDataDir, OutDir, expectedResolutions, postFixSize, expectedPrefix);
+            AssertIconCount(SrcDataDir, OutDir, expectedResolutions);
+        }
+        
+        private void AssertIconsExistAndMatchSize(string testDataDir, string outDir, IList<int> expectedResolutions, bool postfixSize, string expectedPrefix)
+        {
+            DirectoryInfo srcFolderInfo = new DirectoryInfo(testDataDir);
+            foreach (FileInfo file in srcFolderInfo.EnumerateFiles())
+            {
+                foreach (string folder in AndroidResizeService.ResolutionFolders.Keys.ToArray())
+                {
+                    foreach (int expectedResolution in expectedResolutions)
+                    {
+                        int expectedSize = (int) (expectedResolution * AndroidResizeService.ResolutionFolders[folder]);
+                        AssertContainsIconSize(outDir, folder, file.Name, postfixSize, expectedPrefix, expectedResolution,
+                            expectedSize);
+                    }
+                }
+            }
+        }
+
+        private void AssertIconCount(string srcDataDir, string outDir, IList<int> expectedResolutions)
+        {
+            int srcFileCount = Directory.EnumerateFiles(srcDataDir).Count();
+            int iconCount = Directory.EnumerateFiles(outDir, "*", SearchOption.AllDirectories).Count();
+            int expectedCount = srcFileCount * expectedResolutions.Count * AndroidResizeService.ResolutionFolders.Count;
+            Assert.AreEqual(expectedCount, iconCount);
+        }
+
+        private void AssertContainsIconSize(string outputDirectory, string folder, string iconName, bool postfixSize, string suffix, int size, int expectedSize)
+        {
+            string adjustedName = GetIconName(iconName, postfixSize, suffix, size);
+
+            string fullImagePath = Path.Combine(outputDirectory, folder, adjustedName);
+
+            Assert.True(File.Exists(fullImagePath));
+
+            ImageInfo info = _imageResizer.GetInfo(fullImagePath);
+
+            Assert.AreEqual(expectedSize, info.Height);
+            Assert.AreEqual(expectedSize, info.Width);
+        }
+
+        private string GetIconName(string iconName, bool postfixSize, string suffix, int size)
+        {
+
+            string baseName = _renameUtility.ConvertToValidIconName(iconName);
+
+            if (postfixSize)
+            {
+                baseName = _renameUtility.AddPostfix(baseName, $"_{size}");
+            }
+
+            if (!string.IsNullOrEmpty(suffix))
+            {
+                baseName = _renameUtility.AddSuffix(baseName, suffix);
+            }
+
+            return baseName;
+        }
+    }
+}
---
+++ src/IconResizeUtility.Service/AndroidResizeService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace IconResizeUtility.Service
+{
+    public class AndroidResizeService
+    {
+        private readonly ImageResizer _resizer;
+        private readonly RenameUtility _renameUtility;
+        private readonly string _cacheDirectory;
+
+
+        public static IDictionary<string, double> ResolutionFolders = new Dictionary<string, double>
+        {
+            {"drawable-mdpi", 1.0}, // 48*48
+            {"drawable-hdpi", 1.5}, // 72*72
+            {"drawable-xhdpi", 2.0}, // 96*96
+            {"drawable-xxhdpi", 3.0}, // 144*144
+            {"drawable-xxxhdpi", 4.0}, // 192* 192
+        };
+
+        public static IList<int> DefaultRequiredSizes = new List<int>
+        {
+            18,
+            20,
+            24,
+            36,
+            48
+        };
+
+
+        public AndroidResizeService(ImageResizer resizer, RenameUtility renameUtility)
+        {
+            _resizer = resizer;
+            _renameUtility = renameUtility;
+        }
+
+
+        public void Resize(string sourcePath, string destinationPath, bool postfixSize, string suffix, IList<int> requiredSizes)
+        {
+            string[] resolutionFolders = ResolutionFolders.Keys.ToArray();
+
+            EnsureDirectoriesExist(destinationPath, resolutionFolders);
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(sourcePath);
+
+            foreach (FileInfo file in directoryInfo.EnumerateFiles())
+            {
+                foreach (string resolutionFolder in resolutionFolders)
+                {
+                    string baseIconName = _renameUtility.ConvertToValidIconName(file.Name);
+
+                    string path = Path.Combine(destinationPath, resolutionFolder);
+
+                    foreach (int requiredSize in requiredSizes)
+                    {
+                        string finalIconName;
+
+                        if (postfixSize || requiredSizes.Count > 1)
+                        {
+                            finalIconName = _renameUtility.AddPostfix(baseIconName, $"_{requiredSize}");
+                        }
+                        else
+                        {
+                            finalIconName = baseIconName;
+                        }
+
+                        if (!string.IsNullOrEmpty(suffix))
+                        {
+                            finalIconName = _renameUtility.AddSuffix(finalIconName, suffix);
+                        }
+
+                        string destinationIconPath = Path.Combine(path, finalIconName);
+
+                        int size = (int) (requiredSize * ResolutionFolders[resolutionFolder]);
+
+                        _resizer.Resize(file.FullName, destinationIconPath, size, size);
+                    }
+                }
+
+            }
+        }
+
+        private void EnsureDirectoriesExist(string destinationPath, string[] directories)
+        {
+            foreach (string directory in directories)
+            {
+                EnsureDirectoryExists(Path.Combine(destinationPath, directory));
+            }
+        }
+
+        private void EnsureDirectoryExists(string directory)
+        {
+            if (!System.IO.Directory.Exists(directory))
+            {
+                System.IO.Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
