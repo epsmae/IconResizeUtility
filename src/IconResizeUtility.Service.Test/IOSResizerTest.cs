857fa60fe38a393ff16052c70bf45d0db71b80cf
Manuel Eugster <manuel.eugster@hispeed.ch>
2021-02-22 17:17:30 +0100

    Added ios resizer

---
+++ src/IconResizeUtility.Service.Test/IOSResizerTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using IconResizeUtility.TestInfrastructure;
+using NUnit.Framework;
+
+namespace IconResizeUtility.Service.Test
+{
+    public class IOSResizerTest
+    {
+        private ResultChecker _resultChecker;
+        private IOSImageResizer _service;
+
+        private string SrcDataDir
+        {
+            get
+            {
+                return Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Icons");
+            }
+        }
+
+
+        private string OutDir
+        {
+            get
+            {
+                return Path.Combine(TestContext.CurrentContext.WorkDirectory, "out", "Icons");
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            ImageResizer resizer = new ImageResizer();
+            ImageRenamer imageRenamer = new ImageRenamer();
+            _resultChecker = new ResultChecker(resizer, imageRenamer);
+            _service = new IOSImageResizer(resizer, imageRenamer);
+
+
+            if (Directory.Exists(OutDir))
+            {
+                Directory.Delete(OutDir, true);
+            }
+        }
+
+        [Test]
+        public void TestResize()
+        {
+            IList<int> expectedResolutions = IOSImageResizer.DefaultRequiredSizes;
+            string expectedPrefix = "ic_";
+            const bool postFixSize = true;
+
+            _service.Resize(SrcDataDir, OutDir, postFixSize, expectedPrefix, expectedResolutions);
+
+            //_resultChecker.AssertIconsExistAndMatchSize(SrcDataDir, OutDir, expectedResolutions, postFixSize, expectedPrefix);
+            //_resultChecker.AssertIconCount(SrcDataDir, OutDir, expectedResolutions);
+        }
+
+        
+    }
+}
