dd43add7c2f0026ba96f23e5a7e58f35b38a197e
Manuel Eugster <manuel.eugster@hispeed.ch>
2021-03-19 18:31:41 +0100

    Added xamarin sample app

---
+++ playground/ResizeUtility.App/ResizeUtility.App/ResizeUtility.App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ResizeUtility.App.Models;
+using ResizeUtility.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ResizeUtility.App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
